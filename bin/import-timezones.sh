e24acbed25ccf303f8cc541c4139b60f14b4efa2
Joshua Estrin Skrzypek <jskrzypek@gmail.com>
2019-07-07 13:16:46 +0300

    fix(bin): Fix import script

--- bin/import-timezones.sh
+++ bin/import-timezones.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
-BASEDIR=$(dirname "$0")
-export TZ_MONGODB_TIMEZONES_GEOJSON_DEST_PATH="${BASEDIR}/../data/timezones.geojson.zip"
+BASEDIR="$(dirname "$(pwd)/$(readlink "$0" || echo "$0")")/"
+export TZ_MONGODB_TIMEZONES_GEOJSON_DEST_PATH="${TIMEZONES_GEOJSON-${BASEDIR}/../data/timezones.geojson.zip}"
+export MONGO_URL="${MONGO_URL-mongodb://localhost:27017/test}"
 
 if [[ ! -f "${TZ_MONGODB_TIMEZONES_GEOJSON_DEST_PATH}" ]] ; then
   node ./bin/fetch_timezones_geojson.js
@@ -9,7 +10,9 @@ else
   echo "Timezones geojson already downloaded"
 fi
 
+echo "${MONGO_URL}"
+
 cat ${TZ_MONGODB_TIMEZONES_GEOJSON_DEST_PATH} \
   | funzip \
   | node_modules/node-jq/bin/jq '.features | .[] | . * { properties: { timezone: .properties.tzid } }' \
-  | mongoimport --uri ${MONGO_URL-mongodb://localhost:27017/test} -c ${COLLECTION_NAME-timezones}
+  | mongoimport --uri "${MONGO_URL}" -c "${COLLECTION_NAME-timezones}"
