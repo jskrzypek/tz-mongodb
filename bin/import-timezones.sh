26d86d9692468767cca159ae74f41c2fd096a3f1
Joshua Estrin Skrzypek <jskrzypek@gmail.com>
2019-07-07 13:26:09 +0300

    feat(bin): Fix import script

--- bin/import-timezones.sh
+++ bin/import-timezones.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-BASEDIR="$(dirname "$(pwd)/$(readlink "$0" || echo "$0")")/"
-export TZ_MONGODB_TIMEZONES_GEOJSON_DEST_PATH="${TIMEZONES_GEOJSON-${BASEDIR}/../data/timezones.geojson.zip}"
+BASEDIR="$(dirname $(dirname "$(pwd)/$(readlink "$0" || echo "$0")"))/"
+export TZ_MONGODB_TIMEZONES_GEOJSON_DEST_PATH="${TIMEZONES_GEOJSON-${BASEDIR}/data/timezones.geojson.zip}"
 export MONGO_URL="${MONGO_URL-mongodb://localhost:27017/test}"
 
 if [[ ! -f "${TZ_MONGODB_TIMEZONES_GEOJSON_DEST_PATH}" ]] ; then
-  node ./bin/fetch_timezones_geojson.js
+  node "${BASEDIR}/bin/fetch_timezones_geojson.js"
 else
   echo "Timezones geojson already downloaded"
 fi
